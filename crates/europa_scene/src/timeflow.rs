374b6cb3aafa470c0f8d9d452a6aa38121f14cdb
goodguyjay <jair.ramos@live.com>
2025-10-24 22:48:59 -0300

    added timeflow controls

--- crates/europa_scene/src/timeflow.rs
+++ crates/europa_scene/src/timeflow.rs
@@ -25,7 +25,10 @@ impl Plugin for TimeFlowPlugin {
         app.init_resource::<TimeFlow>()
             .init_resource::<SimTime>()
             .configure_sets(Update, SimSet::Advance.before(SimSet::Animate))
-            .add_systems(Update, advance_time.in_set(SimSet::Advance));
+            .add_systems(
+                Update,
+                (advance_time.in_set(SimSet::Advance), timeflow_controls),
+            );
     }
 }
 
@@ -33,6 +36,36 @@ fn advance_time(mut sim: ResMut<SimTime>, tf: Res<TimeFlow>, time: Res<Time>) {
     sim.0 += tf.time_scale * time.delta_secs();
 }
 
-fn time_scale_for_day_duration(minutes_real: f32, europa_day_seconds: f32) -> f32 {
-    europa_day_seconds / (minutes_real * 60.0)
+fn timeflow_controls(keys: Res<ButtonInput<KeyCode>>, mut tf: ResMut<TimeFlow>) {
+    // 1x real time, 600x (10 min/sec), 1200x (5 min/sec), 5000x
+    let mut changed = false;
+
+    if keys.just_pressed(KeyCode::Digit0) || keys.just_pressed(KeyCode::Numpad0) {
+        tf.time_scale = 0.0;
+        changed = true;
+    }
+
+    if keys.just_pressed(KeyCode::Digit1) || keys.just_pressed(KeyCode::Numpad1) {
+        tf.time_scale = 1.0;
+        changed = true;
+    }
+
+    if keys.just_pressed(KeyCode::Digit2) || keys.just_pressed(KeyCode::Numpad2) {
+        tf.time_scale = 600.0;
+        changed = true;
+    }
+
+    if keys.just_pressed(KeyCode::Digit3) || keys.just_pressed(KeyCode::Numpad3) {
+        tf.time_scale = 1200.0;
+        changed = true;
+    }
+
+    if keys.just_pressed(KeyCode::Digit4) || keys.just_pressed(KeyCode::Numpad4) {
+        tf.time_scale = 5000.0;
+        changed = true;
+    }
+
+    if changed {
+        info!("Sim speed set to {}x", tf.time_scale);
+    }
 }
